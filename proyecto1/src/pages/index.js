28fd735e574b953c8c8752d2e0e678e69d3cb8b1
Laureano Rosales <mlaureano06@gmail.com>
2023-02-15 20:56:08 -0300

    Primer commit

---
+++ proyecto1/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Button } from "@mui/material"
+
+export default function Home() {
+  return (
+    <div>
+     Home
+    <Button variant="contained">
+      Click
+    </Button>
+    </div>
+  )
+}
